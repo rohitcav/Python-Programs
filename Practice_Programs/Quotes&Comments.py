d398713088503a70d61e126721ea323d82f6bed7
rohitkumar <rohitcav@gmail.com>
2026-02-06 11:25:10 +0530

    Added new python scripts

---
+++ Practice_Programs/Quotes&Comments.py
@@ -0,0 +1,28 @@
+# This is a single line comment
+
+"""
+This is  multi line comments
+Line number 2
+
+"""
+
+'''
+This is  also multi line comments
+Line number 2
+'''
+
+skill = "Perf Testing"
+print(skill)
+print("skill")
+print('skill')
+
+print("""
+This is  multi line paragraph Quotes
+Line number 2
+""")
+
+print('''
+This is  multi line paragraph Quotes
+Line number 2
+''')
+
---
+++ Practice_Programs/variables.py
@@ -0,0 +1,29 @@
+var1 = "Python"
+var2 = 75
+var3 = '5.6'
+var4 = 4.5
+
+print(var1)
+print(var2)
+print(var3)
+print(var4)
+
+#Multiple assignments
+
+a=b=c=65
+print(a)
+print(b)
+print(c)
+
+# Multiple value assign to multiple variables
+
+x,y,z = "alpha", "beta", 65
+print(x)
+print(y)
+print(z)
+
+# Printing variable's value
+print("Value of x is: ", x)
+print("Value of y is: ", y)
+print("Value of z is: ", z)
+
